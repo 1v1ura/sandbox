ab26e0889cf87cff5d6c15055d41b6946d78cfcd
Takuma Miura <21156330+tkm-mur@users.noreply.github.com>
2019-08-29 17:49:20 +0900

    create layout

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-menu mode="horizontal" :router="true">
+    <el-menu-item index="1" style="pointer-events: none;">
+      Nuxt Diary App
+    </el-menu-item>
+    <el-menu-item index="2" :route="{ path: '/posts/' }">
+      Nuxt Diary App
+    </el-menu-item>
+
+    <no-ssr>
+      <el-menu-item index="4" style="float: right;" :route="{ path: '/' }">
+        <span>ログイン</span>
+      </el-menu-item>
+    </no-ssr>
+
+    <el-menu-item
+      index="5"
+      style="float: right;"
+      :route="{ path: '/posts/new' }"
+      >新規投稿</el-menu-item
+    >
+  </el-menu>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // console.log('test')
+  }
+}
+</script>
