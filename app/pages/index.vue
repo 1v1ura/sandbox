311880c7c969932c3d7881119f871218d0d8a68d
Takuma Miura <21156330+tkm-mur@users.noreply.github.com>
2019-10-13 23:47:11 +0900

    nuxt test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,39 +1,20 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        sandbox
-      </h1>
-      <h2 class="subtitle">
-        My perfect Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h2>カウンター</h2>
+    <h3>Count: {{ count }}</h3>
+    <button type="button" @click="increment">Increment</button>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  components: {
-    Logo
+  computed: {
+    ...mapGetters(['count'])
+  },
+  methods: {
+    ...mapActions(['increment'])
   }
 }
 </script>
@@ -43,6 +24,7 @@ export default {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+  flex-flow: column wrap;
   justify-content: center;
   align-items: center;
   text-align: center;
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 
 export default {
   mode: 'universal',
+  srcDir: 'app',
+
   /*
   ** Headers of the page
   */
